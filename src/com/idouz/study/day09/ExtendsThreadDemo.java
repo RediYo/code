0f68fff04f01e13dd322302153c2aa4d09326c90
idouz <idouz@qq.com>
2019-07-12 11:43:47 +0800

    修改

---
+++ src/com/idouz/study/day09/ExtendsThreadDemo.java
@@ -0,0 +1,34 @@
+package com.idouz.study.day09;
+
+/**
+ * @author idouz
+ * @version 1.0
+ * @date 2019/7/12 11:10
+ */
+public class ExtendsThreadDemo {
+    public static void main(String[] args) {
+        System.out.println("开始了");
+        //创建线程对象
+        MusicThread music = new MusicThread();
+        //启动线程
+        music.start();
+        //主线程中
+        for (int i = 0; i < 50; i++) {
+            System.out.println("打游戏"+i);
+            if(i==10){
+                MusicThread t = new MusicThread();
+                t.start();
+                t.run();//不能使用run
+            }
+        }
+    }
+}
+class MusicThread extends java.lang.Thread{
+    @Override
+    public void run() {
+        super.run();
+        for (int i = 0; i < 50; i++) {
+            System.out.println("听音乐"+i);
+        }
+    }
+}
