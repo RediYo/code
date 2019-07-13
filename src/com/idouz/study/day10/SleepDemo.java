c513bc8c140b9b392aca1f21cb8cba8e64d0dff1
idouz <idouz@qq.com>
2019-07-13 10:23:45 +0800

    Thread 方式二

---
+++ src/com/idouz/study/day10/SleepDemo.java
@@ -0,0 +1,34 @@
+package com.idouz.study.day10;
+
+/**
+ * @author idouz
+ * @version 1.0
+ * @date 2019/7/13 9:26
+ */
+public class SleepDemo {
+    public static void main(String[] args) {
+        Apple a = new Apple();
+        new Thread(a,"小A").start();
+        new Thread(a,"小B").start();
+        new Thread(a,"小C").start();
+
+    }
+}
+class Apple implements Runnable{
+    private Integer nums=50;
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 50; i++) {
+            if (nums > 0) {
+                try {
+                    //子类覆盖父类的方法
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(Thread.currentThread().getName() + "当前拿走第" + nums-- + "苹果");
+            }
+        }
+    }
+}
